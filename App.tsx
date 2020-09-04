b1b39196e9cafeb4cf71aa7777eda5e8419b491b
felipepimsil <felipe.pimsil@gmail.com>
2020-09-04 12:22:22 -0300

    feature/adição do graphQL 2

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import Main from './src/main/main';
-import {ApolloProvider} from '@apollo/client';
 
 declare const global: {HermesInternal: null | {}};
 
